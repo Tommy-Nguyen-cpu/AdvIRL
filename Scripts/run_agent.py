8bfa4395b3e617fc154a1e1587537224cb87ce35
Tommy-Nguyen <75864631+Tommy-Nguyen-cpu@users.noreply.github.com>
2024-04-25 16:01:43 -0400

    Cleaned up execution file
    Nothing is hard coded now. Users are able to pass in various parameters that are important for running reinforcement learning.

--- Scripts/run_agent.py
+++ Scripts/run_agent.py
@@ -7,66 +7,70 @@ import requests
 import json
 import copy
 import numpy as np
-
-pretrained_model = ClipClassifier.CLIP_Classifier("cuda:0")
-
-# Download the ImageNet labels file
-url = "https://raw.githubusercontent.com/anishathalye/imagenet-simple-labels/master/imagenet-simple-labels.json"
-response = requests.get(url)
-labels = response.json()
-
-og_file = "../SavedModel.msgpack"
-output_file = "../TestBanana.msgpack"
-true_class = "banana"
-target_class = "slug"
-path_to_og_images = "../BananaScene/Output/"
-transforms_path =  "../BananaScene/transforms.json"
-short_transforms_path = "../BananaScene/short_transforms.json"
-images_output_path = "../BananaScene/AdvOut/"
-num_imgs = 20
+import argparse
 
 
-def generate_transforms(og_transforms_path, output_transforms_path, number_of_transforms= 10):
-    file = open(og_transforms_path)
-    transforms = json.load(file)
-    copyJson = copy.deepcopy(transforms)
-    with open(output_transforms_path, "w") as short_output:
-        copyJson['frames'] = list(np.array(copyJson['frames'])[:number_of_transforms])
-        json.dump(copyJson, short_output)
+def main(args):
+    print("temp")
+    pretrained_model = ClipClassifier.CLIP_Classifier(args.clip_device)
 
-generate_transforms(transforms_path, short_transforms_path, number_of_transforms=31)
+    # Download the ImageNet labels file
+    url = "https://raw.githubusercontent.com/anishathalye/imagenet-simple-labels/master/imagenet-simple-labels.json"
+    response = requests.get(url)
+    labels = response.json()
+    def generate_transforms(og_transforms_path, output_transforms_path, number_of_transforms= 10):
+        file = open(og_transforms_path)
+        transforms = json.load(file)
+        copyJson = copy.deepcopy(transforms)
+        with open(output_transforms_path, "w") as short_output:
+            copyJson['frames'] = list(np.array(copyJson['frames'])[:number_of_transforms])
+            json.dump(copyJson, short_output)
 
-nerf_model = NeRF_Model(og_file, output_file, path_to_og_images, short_transforms_path, images_output_path)
+    generate_transforms(args.transforms_path, args.output_transforms_path, number_of_transforms=args.num_imgs)
 
-# Create environment instance
-env = InstantNGPEnv(pretrained_model, nerf_model, true_class, target_class, num_imgs,(800,800), path_to_og_images, images_output_path, output_file, short_transforms_path, labels)
+    nerf_model = NeRF_Model(args.input_saved_nerf_file_path, args.output_saved_nerf_file_path, args.input_image_folder, args.output_transforms_path, args.output_image_folder)
 
-print("GOT TO PPO!")
-# Create RL agent
-model = PPO('MlpPolicy', env, device='cpu', n_steps=2, batch_size=2, max_grad_norm = .00001)
+    # Create environment instance
+    env = InstantNGPEnv(pretrained_model, nerf_model, args.true_class, args.target_class, args.num_imgs,(args.image_width,args.image_height), args.input_image_folder, args.output_image_folder, args.output_saved_nerf_file_path, args.output_transforms_path, labels)
 
-print("GOT AFTER PPO!")
-# Train the agent
-# Evaluate the trained agent
-episode_reward = 0
-obs, info = env.reset()
-_states = None
+    # Create RL agent
+    model = PPO('MlpPolicy', env, device='cpu', n_steps=2, batch_size=2, max_grad_norm = .00001)
+    # Train the agent
+    # Evaluate the trained agent
+    episode_reward = 0
+    obs, info = env.reset()
+    _states = None
 
-max_num_imgs = 0
+    while True:
+        model.learn(total_timesteps=1)
+        action, _states = model.predict(obs, _states)
 
-epochs = 0
-while True:
-    model.learn(total_timesteps=1)
-    action, _states = model.predict(obs, _states)
+        nerf_model.render_outputs(args.output_saved_nerf_file_path, args.output_transforms_path, args.output_image_folder, width=args.image_width, height=args.image_height)
 
-    nerf_model.render_outputs(output_file, transforms_path, images_output_path, width=env.ImageHeight, height=env.ImageWidth)
+        _, _, _, reward, info = env.pred_labels()
 
-    _, _, _, reward, info = env.pred_labels()
+        episode_reward += reward
+        print("Current episode reward: " + str(episode_reward))
 
-    episode_reward += reward
-    print("Current episode reward: " + str(episode_reward))
+        # Be careful where you put this if block. If you put it after the "reset environment" if block, it might throw an error, because "reset" returns an empty dictionary.
+        if (info['Target'] == info['Predicted'] and info['Confidence'] >= .65 and info["Num_Imgs"] >= 15):
+            print("Episode reward:", episode_reward)
+            break
 
-    # Be careful where you put this if block. If you put it after the "reset environment" if block, it might throw an error, because "reset" returns an empty dictionary.
-    if (info['Target'] == info['Predicted'] and info['Confidence'] >= .65 and info["Num_Imgs"] >= 15):
-        print("Episode reward:", episode_reward)
-        break
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Reinforcement learning approach for generating adversarial noise.")
+    parser.add_argument("--clip_device", default="cuda:0", type=str, help="Which device to execute clip on.")
+    parser.add_argument("--input_image_folder", default="../BananaScene/Output/", type=str, help="Path to the images we will use to generate a 3D model using NeRF.")
+    parser.add_argument("--output_image_folder", default="../BananaScene/AdvOut/", type=str, help="Folder to output generated images to.")
+    parser.add_argument("--image_width", default=800, type=int, help="Width of rendered image from NeRF.")
+    parser.add_argument("--image_height", default=800, type=int, help="Width of rendered image from NeRF.")
+    parser.add_argument("--transforms_path", default="../BananaScene/transforms.json", type=str, help="Path to the original JSON path")
+    parser.add_argument("--output_transforms_path", default="../BananaScene/short_transforms.json", type=str, help="Output path we will store the modified transforms file.")
+    parser.add_argument("--input_saved_nerf_file_path", default="../SavedModel.msgpack", type=str, help="Path to our input saved NeRF model file.")
+    parser.add_argument("--output_saved_nerf_file_path", default="../TestBanana.msgpack", type=str, help="Path to where we will save our generated NeRF model file.")
+
+    parser.add_argument("--true_class", default="banana", type=str, help="True class of the object.")
+    parser.add_argument("--target_class", default="slug", type=str, help="Target class to produce adversarial noise to target.")
+    parser.add_argument("--num_imgs", default=20, type=int, help="Number of images to use as observation space for reinforcement learning.")
+
+    main(parser)
